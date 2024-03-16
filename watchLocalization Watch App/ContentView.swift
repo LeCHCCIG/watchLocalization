b07b52e99680c6ae316dbdf0b38d4bef05f04233
kehinde-elelu <abdulsalamelelu@gmail.com>
2024-03-15 21:45:00 -0400

    Initial Commit

---
+++ watchLocalization Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchLocalization Watch App
+//
+//  Created by Kehinde  Elelu on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
